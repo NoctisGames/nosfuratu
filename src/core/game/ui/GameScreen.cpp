21856e1309f96e29093d8a756d9c3d3ca12a22ff
Stephen Gowen <dev.sgowen@gmail.com>
2016-02-12 14:04:52 -0600

    more tweaks

--- src/core/game/ui/GameScreen.cpp
+++ src/core/game/ui/GameScreen.cpp
@@ -11,7 +11,7 @@
 #include "GameScreenLevels.h"
 #include "Game.h"
 
-GameScreen::GameScreen() : m_fFPSStateTime(0), m_iFrames(0), m_iFPS(0), m_fDeltaTime(0), m_fScreenHeldTime(0), m_isRequestingRender(false), m_iRequestedAction(REQUESTED_ACTION_UPDATE), m_iNumFramesToDiscard(60), m_isPaused(false), m_isScreenHeldDown(false)
+GameScreen::GameScreen() : m_fFPSStateTime(0), m_iFrames(0), m_iFPS(0), m_fDeltaTime(0), m_fScreenHeldTime(0), m_isRequestingRender(false), m_iRequestedAction(REQUESTED_ACTION_UPDATE), m_iNumFramesToDiscard(0), m_isPaused(false), m_isScreenHeldDown(false)
 {
     m_touchPoint = std::unique_ptr<Vector2D>(new Vector2D());
     m_touchPointDown = std::unique_ptr<Vector2D>(new Vector2D());
