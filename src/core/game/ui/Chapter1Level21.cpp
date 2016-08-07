6159eb3c62898cbe13b11734ffcdc25e49292820
Stephen Gowen <dev.sgowen@gmail.com>
2016-08-07 15:36:51 -0500

    Stub Level 21

--- src/core/game/ui/Chapter1Level21.cpp
+++ src/core/game/ui/Chapter1Level21.cpp
@@ -13,11 +13,11 @@
 #include "Vector2D.h"
 #include "Game.h"
 
-/// Chapter 1 Level 21 End Boss ///
+/// Chapter 1 Level 21 ///
 
 Chapter1Level21 * Chapter1Level21::getInstance()
 {
-    static Chapter1Level21 *instance = new Chapter1Level21("{\"world\":1,\"level\":21,\"midgrounds\":[{\"gridX\":0,\"gridY\":96,\"type\":2},{\"gridX\":80,\"gridY\":96,\"type\":0},{\"gridX\":128,\"gridY\":96,\"type\":1},{\"gridX\":200,\"gridY\":96,\"type\":2},{\"gridX\":264,\"gridY\":96,\"type\":0},{\"gridX\":296,\"gridY\":96,\"type\":2},{\"gridX\":354,\"gridY\":96,\"type\":1}],\"grounds\":[{\"gridX\":0,\"gridY\":0,\"type\":25},{\"gridX\":8,\"gridY\":0,\"type\":28},{\"gridX\":136,\"gridY\":0,\"type\":28},{\"gridX\":264,\"gridY\":0,\"type\":28},{\"gridX\":392,\"gridY\":0,\"type\":28}],\"pits\":[],\"exitGrounds\":[],\"holes\":[],\"foregroundObjects\":[{\"gridX\":386,\"gridY\":96,\"type\":12}],\"midBossForegroundObjects\":[],\"countHissWithMinas\":[{\"gridX\":353,\"gridY\":96}],\"enemies\":[],\"collectibles\":[{\"gridX\":80,\"gridY\":104,\"type\":0},{\"gridX\":104,\"gridY\":104,\"type\":0},{\"gridX\":128,\"gridY\":104,\"type\":0}],\"jons\":[{\"gridX\":32,\"gridY\":120}],\"extraForegroundObjects\":[],\"markers\":[{\"gridX\":392,\"gridY\":0,\"type\":0},{\"gridX\":520,\"gridY\":0,\"type\":1}]}");
+    static Chapter1Level21 *instance = new Chapter1Level21("{\"world\":1,\"level\":21,\"midgrounds\":[],\"grounds\":[{\"gridX\":0,\"gridY\":0,\"type\":25},{\"gridX\":8,\"gridY\":0,\"type\":28},{\"gridX\":136,\"gridY\":0,\"type\":28},{\"gridX\":264,\"gridY\":0,\"type\":28},{\"gridX\":392,\"gridY\":0,\"type\":28},{\"gridX\":520,\"gridY\":0,\"type\":28},{\"gridX\":648,\"gridY\":0,\"type\":28},{\"gridX\":776,\"gridY\":0,\"type\":28},{\"gridX\":904,\"gridY\":0,\"type\":29}],\"pits\":[],\"exitGrounds\":[],\"holes\":[],\"foregroundObjects\":[{\"gridX\":776,\"gridY\":96,\"type\":12}],\"midBossForegroundObjects\":[],\"countHissWithMinas\":[{\"gridX\":744,\"gridY\":96}],\"enemies\":[],\"collectibles\":[],\"jons\":[{\"gridX\":28,\"gridY\":104}],\"extraForegroundObjects\":[],\"markers\":[]}");
     
     return instance;
 }
