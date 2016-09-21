3b72f50ea60bc0a5a6cad6c137a0d7f677d8b414
Stephen Gowen <dev.sgowen@gmail.com>
2016-09-21 01:45:15 -0500

    v0.7.0

--- src/core/game/GameConstants.h
+++ src/core/game/GameConstants.h
@@ -12,8 +12,8 @@
 //// VERSION ////
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 6
-#define VERSION_BUILD 1
+#define VERSION_MINOR 7
+#define VERSION_BUILD 0
 
 //// Requested Action Definitions ////
 
