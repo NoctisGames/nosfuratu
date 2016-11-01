34aaffd912642619c5e94b9909a13b5f2ba094ca
Stephen Gowen <dev.sgowen@gmail.com>
2016-11-01 10:52:22 -0500

    Fixing silly mistake I made regarding setting a local bool isMobile instead of the class member variable m_isWindowsMobile

--- src/core/game/ui/direct3d/Direct3DFramebufferRadialBlurGpuProgramWrapper.cpp
+++ src/core/game/ui/direct3d/Direct3DFramebufferRadialBlurGpuProgramWrapper.cpp
@@ -15,14 +15,13 @@ using namespace Windows::System::Profile;
 
 Direct3DFramebufferRadialBlurGpuProgramWrapper::Direct3DFramebufferRadialBlurGpuProgramWrapper(const std::shared_ptr<DX::DeviceResources>& deviceResources) : FramebufferRadialBlurGpuProgramWrapper(), m_iNumShadersLoaded(0), m_isWindowsMobile(false), m_deviceResources(deviceResources)
 {
-	bool isMobile;
 #if defined NG_WIN_10
 	AnalyticsVersionInfo^ api = AnalyticsInfo::VersionInfo;
-	isMobile = api->DeviceFamily->Equals("Windows.Mobile");
+	m_isWindowsMobile = api->DeviceFamily->Equals("Windows.Mobile");
 #elif defined NG_WIN_8
-	isMobile = false;
+	m_isWindowsMobile = false;
 #elif defined NG_WIN_PHONE_8
-	isMobile = true;
+	m_isWindowsMobile = true;
 #endif
     
     createConstantBuffers();
