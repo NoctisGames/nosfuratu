1d0af60a9e2e1cdf2b2611a2511ddb16cf336ade
Stephen Gowen <dev.sgowen@gmail.com>
2016-12-06 17:46:03 -0600

    Fixing screen blending (noticed this while testing on my Mac Thunderbolt display)

--- src/core/framework/ui/opengl/OpenGLManager.cpp
+++ src/core/framework/ui/opengl/OpenGLManager.cpp
@@ -114,7 +114,7 @@ void OpenGLManager::useNormalBlending()
 
 void OpenGLManager::useScreenBlending()
 {
-    glBlendFuncSeparate(GL_ONE, GL_ZERO, GL_ONE, GL_ZERO);
+    glBlendFuncSeparate(GL_ONE, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
 }
 
