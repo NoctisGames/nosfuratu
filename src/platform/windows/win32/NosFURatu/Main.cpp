a64f913e53ed6c226b42e869d3cc8174a1d0d497
Stephen Gowen <dev.sgowen@gmail.com>
2017-02-21 11:28:42 -0600

    Forgot to add Main.cpp from Dante

--- src/platform/windows/win32/NosFURatu/Main.cpp
+++ src/platform/windows/win32/NosFURatu/Main.cpp
@@ -4,6 +4,7 @@
 
 #include "pch.h"
 #include "Direct3DMain.h"
+#include <Dbt.h>
 
 using namespace DirectX;
 
@@ -27,12 +28,16 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    if (!XMVerifyCPUSupport())
-        return 1;
+	if (!XMVerifyCPUSupport())
+	{
+		return 1;
+	}
 
     HRESULT hr = CoInitializeEx(nullptr, COINITBASE_MULTITHREADED);
-    if (FAILED(hr))
-        return 1;
+	if (FAILED(hr))
+	{
+		return 1;
+	}
 
     g_main = std::make_unique<Direct3DMain>();
 
@@ -52,8 +57,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         wcex.lpszMenuName = nullptr;
         wcex.lpszClassName = L"NosFURatuWindowClass";
         wcex.hIconSm = LoadIcon(wcex.hInstance, L"IDI_ICON");
-        if (!RegisterClassEx(&wcex))
-            return 1;
+		if (!RegisterClassEx(&wcex))
+		{
+			return 1;
+		}
 
         // Create window
         int w, h;
@@ -73,39 +80,56 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         // TODO: Change to CreateWindowEx(WS_EX_TOPMOST, L"NosFURatuWindowClass", L"NosFURatu", WS_POPUP,
         // to default to fullscreen.
 
-        if (!hwnd)
-            return 1;
+		if (!hwnd)
+		{
+			return 1;
+		}
 
-        ShowWindow(hwnd, nCmdShow);
-        // TODO: Change nCmdShow to SW_SHOWMAXIMIZED to default to fullscreen.
+        ShowWindow(hwnd, SW_SHOWMAXIMIZED);
 
         SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(g_main.get()) );
 
         GetClientRect(hwnd, &rc);
 
         g_main->Initialize(hwnd, rc.right - rc.left, rc.bottom - rc.top);
+
+		HDEVNOTIFY hNewAudio = nullptr;
+
+		// Listen for new audio devices
+		DEV_BROADCAST_DEVICEINTERFACE filter = { 0 };
+		filter.dbcc_size = sizeof(filter);
+		filter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+		filter.dbcc_classguid = KSCATEGORY_AUDIO;
+
+		hNewAudio = RegisterDeviceNotification(hwnd, &filter,
+			DEVICE_NOTIFY_WINDOW_HANDLE);
+
+		if (hNewAudio)
+		{
+			UnregisterDeviceNotification(hNewAudio);
+		}
     }
 
-    // Main message loop
-    MSG msg = { 0 };
-    while (WM_QUIT != msg.message)
-    {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else
-        {
-            g_main->Tick();
-        }
-    }
+	// Main message loop
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			g_main->Tick();
+		}
+	}
 
-    g_main.reset();
+	g_main.reset();
 
-    CoUninitialize();
+	CoUninitialize();
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 // Windows procedure
@@ -117,8 +141,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     static bool s_in_sizemove = false;
     static bool s_in_suspend = false;
     static bool s_minimized = false;
-    static bool s_fullscreen = false;
-    // TODO: Set s_fullscreen to true if defaulting to fullscreen.
+    static bool s_fullscreen = true;
 
     auto direct3DMain = reinterpret_cast<Direct3DMain*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
 
@@ -135,16 +158,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             if (!s_minimized)
             {
                 s_minimized = true;
-                if (!s_in_suspend && direct3DMain)
-                    direct3DMain->OnSuspending();
+				if (!s_in_suspend && direct3DMain)
+				{
+					direct3DMain->OnSuspending();
+				}
                 s_in_suspend = true;
             }
         }
         else if (s_minimized)
         {
             s_minimized = false;
-            if (s_in_suspend && direct3DMain)
-                direct3DMain->OnResuming();
+			if (s_in_suspend && direct3DMain)
+			{
+				direct3DMain->OnResuming();
+			}
             s_in_suspend = false;
         }
         else if (!s_in_sizemove && direct3DMain)
@@ -188,22 +215,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 direct3DMain->OnDeactivated();
             }
         }
-        break;
+
+		Keyboard::ProcessMessage(message, wParam, lParam);
+		Mouse::ProcessMessage(message, wParam, lParam);
+        
+		break;
 
     case WM_POWERBROADCAST:
         switch (wParam)
         {
         case PBT_APMQUERYSUSPEND:
-            if (!s_in_suspend && direct3DMain)
-                direct3DMain->OnSuspending();
+			if (!s_in_suspend && direct3DMain)
+			{
+				direct3DMain->OnSuspending();
+			}
             s_in_suspend = true;
             return TRUE;
 
         case PBT_APMRESUMESUSPEND:
             if (!s_minimized)
             {
-                if (s_in_suspend && direct3DMain)
-                    direct3DMain->OnResuming();
+				if (s_in_suspend && direct3DMain)
+				{
+					direct3DMain->OnResuming();
+				}
                 s_in_suspend = false;
             }
             return TRUE;
@@ -211,7 +246,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
 
     case WM_DESTROY:
-        PostQuitMessage(0);
+		PostQuitMessage(0);
         break;
 
     case WM_SYSKEYDOWN:
@@ -225,8 +260,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
                 int width = 800;
                 int height = 600;
-                if (direct3DMain)
-                    direct3DMain->GetDefaultSize(width, height);
+				if (direct3DMain)
+				{
+					direct3DMain->GetDefaultSize(width, height);
+				}
 
                 ShowWindow(hWnd, SW_SHOWNORMAL);
 
@@ -244,20 +281,58 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
             s_fullscreen = !s_fullscreen;
         }
+
+		Keyboard::ProcessMessage(message, wParam, lParam);
+
         break;
 
     case WM_MENUCHAR:
         // A menu is active and the user presses a key that does not correspond
         // to any mnemonic or accelerator key. Ignore so we don't produce an error beep.
         return MAKELRESULT(0, MNC_CLOSE);
+
+	case WM_INPUT:
+	case WM_MOUSEMOVE:
+	case WM_LBUTTONDOWN:
+	case WM_LBUTTONUP:
+	case WM_RBUTTONDOWN:
+	case WM_RBUTTONUP:
+	case WM_MBUTTONDOWN:
+	case WM_MBUTTONUP:
+	case WM_MOUSEWHEEL:
+	case WM_XBUTTONDOWN:
+	case WM_XBUTTONUP:
+	case WM_MOUSEHOVER:
+		Mouse::ProcessMessage(message, wParam, lParam);
+		break;
+
+	case WM_KEYDOWN:
+	case WM_KEYUP:
+	case WM_SYSKEYUP:
+		Keyboard::ProcessMessage(message, wParam, lParam);
+		break;
+
+	case WM_DEVICECHANGE:
+		if (wParam == DBT_DEVICEARRIVAL)
+		{
+			auto pDev = reinterpret_cast<PDEV_BROADCAST_HDR>(lParam);
+			if (pDev)
+			{
+				if (pDev->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE)
+				{
+					auto pInter = reinterpret_cast<const PDEV_BROADCAST_DEVICEINTERFACE>(pDev);
+					if (pInter->dbcc_classguid == KSCATEGORY_AUDIO)
+					{
+						if (direct3DMain)
+						{
+							direct3DMain->OnNewAudioDevice();
+						}
+					}
+				}
+			}
+		}
+		return 0;
     }
 
     return DefWindowProc(hWnd, message, wParam, lParam);
-}
-
-
-// Exit helper
-void ExitGame()
-{
-    PostQuitMessage(0);
 }
