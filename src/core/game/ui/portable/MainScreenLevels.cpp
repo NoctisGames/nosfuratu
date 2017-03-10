1743dea17ec5f396d133e609f4ccb1a68b757062
Stephen Gowen <dev.sgowen@gmail.com>
2017-03-10 16:09:17 -0600

    Revert "Removing call to stop all sousd in MainScreenLevels"
    This reverts commit d11fa283b1a91b10942fcb79bdde5140cd17d46d.

--- src/core/game/ui/portable/MainScreenLevels.cpp
+++ src/core/game/ui/portable/MainScreenLevels.cpp
@@ -146,6 +146,8 @@ void Level::exit(MainScreen* ms)
     
     m_sourceGame = nullptr;
     
+    NG_AUDIO_ENGINE->stopAllSounds();
+    
     if (m_playLevelSelectMusicOnExit)
     {
         NG_AUDIO_ENGINE->loadMusic("level_select_bgm");
