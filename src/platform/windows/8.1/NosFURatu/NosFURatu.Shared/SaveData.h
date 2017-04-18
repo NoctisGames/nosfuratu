e7bc1b638806d3ba8e31a6138b9ab091a9171220
Stephen Gowen <dev.sgowen@gmail.com>
2017-04-18 18:10:47 -0500

    Not really sure what I was thinking. Makes much more sense to just use the old 8.1 code as a starting point.

---
+++ src/platform/windows/8.1/NosFURatu/NosFURatu.Shared/SaveData.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace NosFURatu
+{
+	class SaveData
+	{
+	public:
+		static int getNumGoldenCarrots();
+        static void setNumGoldenCarrots(int numGoldenCarrots);
+        
+        static int getViewedCutscenesFlag();
+        static void setViewedCutscenesFlag(int viewedCutscenesFlag);
+        
+        static int getJonUnlockedAbilitiesFlag();
+        
+        static int getLevelScore(int world, int level);
+
+        static int getLevelStatsFlag(int world, int level);
+        static void setLevelStatsFlag(int world, int level, int levelStatsFlag);
+        
+        static void setLevelComplete(int world, int level, int score, int levelStatsFlag, int jonUnlockedAbilitiesFlag);
+        
+        static int getScorePushedOnline(int world, int level);
+        static void setScorePushedOnline(int world, int level, int score);
+
+	private:
+        static Platform::String^ getKeyForNumGoldenCarrots();
+        static Platform::String^ getKeyForJonUnlockedAbilitiesFlag();
+        static Platform::String^ getKeyForViewedCutscenesFlag();
+        
+		static Platform::String^ getKeyForLevelScore(int world, int level);
+        static Platform::String^ getKeyForLevelStats(int world, int level);
+        static Platform::String^ getKeyForLevelOnlineScore(int world, int level);
+
+		SaveData() {}
+		~SaveData() {}
+	};
+}
