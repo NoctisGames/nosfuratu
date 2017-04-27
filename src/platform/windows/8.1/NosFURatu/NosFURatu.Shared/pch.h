9b1571857420ba6e746434945b03be6b299c7123
Stephen Gowen <dev.sgowen@gmail.com>
2017-04-27 16:14:12 -0500

    wip

--- src/platform/windows/8.1/NosFURatu/NosFURatu.Shared/pch.h
+++ src/platform/windows/8.1/NosFURatu/NosFURatu.Shared/pch.h
@@ -7,14 +7,16 @@
 
 #include <wrl.h>
 #include <wrl/client.h>
-#include <dxgi1_4.h>
-#include <d3d11_3.h>
-#include <d2d1_3.h>
-#include <d2d1effects_2.h>
-#include <dwrite_3.h>
+#include <d3d11_2.h>
+#include <d2d1_2.h>
+#include <d2d1effects_1.h>
+#include <dwrite_2.h>
 #include <wincodec.h>
 #include <DirectXColors.h>
 #include <DirectXMath.h>
 #include <memory>
 #include <agile.h>
 #include <concrt.h>
+#include <collection.h>
+#include <ppltasks.h>
+#include "App.xaml.h"
