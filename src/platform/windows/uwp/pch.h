94cb571f69902cd4e1bbf398a3471c1e994d6648
Stephen Gowen <dev.sgowen@gmail.com>
2017-04-27 15:46:11 -0500

    Windows 10 UWP is working now

--- src/platform/windows/uwp/pch.h
+++ src/platform/windows/uwp/pch.h
@@ -9,20 +9,22 @@
 #define NOMINMAX
 
 #include <wrl.h>
+#include <wrl/client.h>
+#include <wincodec.h>
+#include <concrt.h>
+#include <ppltasks.h>
+#include <winapifamily.h>
 
-#include <d3d11_3.h>
-#include <dxgi1_4.h>
 #include <DirectXMath.h>
 #include <DirectXColors.h>
 
-#include <algorithm>
-#include <exception>
-#include <memory>
-#include <stdexcept>
+#include <d3d11_3.h>
+#include <dxgi1_4.h>
 
-#include <stdio.h>
 #include <pix.h>
 
 #ifdef _DEBUG
-	#include <dxgidebug.h>
+#include <dxgidebug.h>
 #endif
+
+#include <Windows.UI.Core.h>
