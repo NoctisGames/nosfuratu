ed57cfdd81f3cf5f120482e8b96dd95494fd9288
Stephen Gowen <dev.sgowen@gmail.com>
2017-05-03 11:21:01 -0500

    OpenGL shaders are now encrypted as well

---
+++ encrypt_textures.sh
@@ -0,0 +1,80 @@
+cd assets/textures/
+
+../../XOREncryptDecrypt texture_001.png texture_001.ngt
+../../XOREncryptDecrypt texture_002.png texture_002.ngt
+../../XOREncryptDecrypt texture_003.png texture_003.ngt
+../../XOREncryptDecrypt texture_004.png texture_004.ngt
+../../XOREncryptDecrypt texture_005.png texture_005.ngt
+../../XOREncryptDecrypt texture_006.png texture_006.ngt
+../../XOREncryptDecrypt texture_007.png texture_007.ngt
+../../XOREncryptDecrypt texture_008.png texture_008.ngt
+../../XOREncryptDecrypt texture_009.png texture_009.ngt
+../../XOREncryptDecrypt texture_010.png texture_010.ngt
+../../XOREncryptDecrypt texture_011.png texture_011.ngt
+../../XOREncryptDecrypt texture_012.png texture_012.ngt
+../../XOREncryptDecrypt texture_013.png texture_013.ngt
+../../XOREncryptDecrypt texture_014.png texture_014.ngt
+../../XOREncryptDecrypt texture_015.png texture_015.ngt
+../../XOREncryptDecrypt texture_016.png texture_016.ngt
+../../XOREncryptDecrypt texture_017.png texture_017.ngt
+../../XOREncryptDecrypt texture_018.png texture_018.ngt
+../../XOREncryptDecrypt texture_019.png texture_019.ngt
+../../XOREncryptDecrypt texture_020.png texture_020.ngt
+../../XOREncryptDecrypt texture_021.png texture_021.ngt
+../../XOREncryptDecrypt texture_022.png texture_022.ngt
+../../XOREncryptDecrypt texture_023.png texture_023.ngt
+../../XOREncryptDecrypt texture_024.png texture_024.ngt
+../../XOREncryptDecrypt texture_025.png texture_025.ngt
+
+cd compressed
+
+../../../XOREncryptDecrypt texture_026.png texture_026.ngt
+../../../XOREncryptDecrypt texture_027.png texture_027.ngt
+../../../XOREncryptDecrypt texture_028.png texture_028.ngt
+../../../XOREncryptDecrypt texture_029.png texture_029.ngt
+../../../XOREncryptDecrypt texture_030.png texture_030.ngt
+../../../XOREncryptDecrypt texture_031.png texture_031.ngt
+../../../XOREncryptDecrypt texture_032.png texture_032.ngt
+../../../XOREncryptDecrypt texture_033.png texture_033.ngt
+../../../XOREncryptDecrypt texture_034.png texture_034.ngt
+../../../XOREncryptDecrypt texture_035.png texture_035.ngt
+../../../XOREncryptDecrypt texture_036.png texture_036.ngt
+../../../XOREncryptDecrypt texture_037.png texture_037.ngt
+../../../XOREncryptDecrypt texture_038.png texture_038.ngt
+../../../XOREncryptDecrypt texture_039.png texture_039.ngt
+../../../XOREncryptDecrypt texture_040.png texture_040.ngt
+../../../XOREncryptDecrypt texture_041.png texture_041.ngt
+
+cd ../desktop
+
+../../../XOREncryptDecrypt texture_042.png texture_042.ngt
+../../../XOREncryptDecrypt texture_043.png texture_043.ngt
+
+cd ../level_editor
+
+../../../XOREncryptDecrypt texture_044.png texture_044.ngt
+
+cd ../../../src/core/framework/graphics/opengl/shader
+
+../../../../../../XOREncryptDecrypt shader_001_frag.fsh shader_001_frag.ngs
+../../../../../../XOREncryptDecrypt shader_002_frag.fsh shader_002_frag.ngs
+../../../../../../XOREncryptDecrypt shader_003_frag.fsh shader_003_frag.ngs
+../../../../../../XOREncryptDecrypt shader_001_vert.vsh shader_001_vert.ngs
+../../../../../../XOREncryptDecrypt shader_002_vert.vsh shader_002_vert.ngs
+../../../../../../XOREncryptDecrypt shader_003_vert.vsh shader_003_vert.ngs
+
+cd ../../../../game/graphics/opengl/shader
+
+../../../../../../XOREncryptDecrypt shader_004_frag.fsh shader_004_frag.ngs
+../../../../../../XOREncryptDecrypt shader_005_frag.fsh shader_005_frag.ngs
+../../../../../../XOREncryptDecrypt shader_006_frag.fsh shader_006_frag.ngs
+../../../../../../XOREncryptDecrypt shader_007_frag.fsh shader_007_frag.ngs
+../../../../../../XOREncryptDecrypt shader_008_frag.fsh shader_008_frag.ngs
+../../../../../../XOREncryptDecrypt shader_009_frag.fsh shader_009_frag.ngs
+../../../../../../XOREncryptDecrypt shader_010_frag.fsh shader_010_frag.ngs
+../../../../../../XOREncryptDecrypt shader_011_frag.fsh shader_011_frag.ngs
+../../../../../../XOREncryptDecrypt shader_012_frag.fsh shader_012_frag.ngs
+../../../../../../XOREncryptDecrypt shader_013_frag.fsh shader_013_frag.ngs
+../../../../../../XOREncryptDecrypt shader_014_frag.fsh shader_014_frag.ngs
+../../../../../../XOREncryptDecrypt shader_008_vert.vsh shader_008_vert.ngs
+../../../../../../XOREncryptDecrypt shader_010_vert.vsh shader_010_vert.ngs
